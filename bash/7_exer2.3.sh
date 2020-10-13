de905d03637426e4273681b49f76b6cc4ea809dd
Bruce Wang <taolinwant@gmail.com>
2020-10-13 11:48:29 +0800

    bash syntax and other command.

---
+++ bash/7_exer2.3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Home direcotry:"
+echo $HOME
+echo
+
+echo "terminal type:"
+echo $TERM
+echo
+
+echo
+echo
+echo `ls /etc/rc3.d/S*`
---
+++ bash/8_special_parameter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# run this bash script with at least one parameters.
+echo "$*"
+echo '$*'
+
+echo "$@"
+
+
+echo "$#"
+
+
+echo "$?"
+
+
+echo "\$-: $-"
+
+
+echo "$0"
+
+
+echo "$_"
